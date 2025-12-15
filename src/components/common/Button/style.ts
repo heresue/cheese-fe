d29c81fa1613df3eb244cf94628a9d669ab78cb9
sue <sue.synpark@gmail.com>
2025-12-16 02:27:52 +0900

    style: 공통 버튼 컴포넌트 disabled 시 opacity로 색상 변경

--- src/components/common/Button/style.ts
+++ src/components/common/Button/style.ts
@@ -2,7 +2,7 @@ import type { CSSProperties } from 'react';
 import type { ButtonProps } from './type';
 
 const base =
-  'inline-flex items-center justify-center font-bold disabled:cursor-not-allowed disabled:primary-50';
+  'inline-flex items-center justify-center font-bold disabled:cursor-not-allowed disabled:opacity-50';
 
 export function getButtonStyle({
   variant = 'contained',
