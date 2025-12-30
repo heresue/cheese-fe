67e81d9aa3810e2474a4a92ebeec761a22770486
sue <sue.synpark@gmail.com>
2025-12-30 09:53:37 +0900

    chore: public 라우트 폴더 구조 세팅

---
+++ src/app/(public)/(auth)/find-id/page.tsx
@@ -0,0 +1,3 @@
+export default function FindIdPage() {
+  return <h1 className="text-4xl">find-id page</h1>;
+}
---
+++ src/app/(public)/splash/page.tsx
@@ -0,0 +1,7 @@
+export default function FindIdPage() {
+  return (
+    <div className="flex min-h-dvh items-center justify-center">
+      <h1 className="text-4xl">splash page</h1>;
+    </div>
+  );
+}
