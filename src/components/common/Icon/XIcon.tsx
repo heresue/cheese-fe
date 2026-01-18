d06779fc44ceb1fedbf0407ee8be7630d5e37cb1
sue <sue.synpark@gmail.com>
2026-01-19 02:59:37 +0900

    chore: 닫기 아이콘 컴포넌트 추가

---
+++ src/components/common/Icon/XIcon.tsx
@@ -0,0 +1,13 @@
+import clsx from 'clsx';
+
+type IconProps = {
+  className?: string;
+};
+
+export default function XIcon({ className }: IconProps) {
+  return (
+    <svg viewBox="0 0 14 14" fill="currentColor" className={clsx('shrink-0', className)}>
+      <path d="M1.4 14L0 12.6L5.6 7L0 1.4L1.4 0L7 5.6L12.6 0L14 1.4L8.4 7L14 12.6L12.6 14L7 8.4L1.4 14Z" />
+    </svg>
+  );
+}
