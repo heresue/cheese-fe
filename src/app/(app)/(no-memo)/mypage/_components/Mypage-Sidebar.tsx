14500984421f10a0edda893587ac57d60e737b99
sue <sue.synpark@gmail.com>
2026-04-02 00:39:35 +0900

    feat: 마이페이지 사이드바 UI 구현

--- src/app/(app)/(no-memo)/mypage/_components/Mypage-Sidebar.tsx
+++ src/app/(app)/(no-memo)/mypage/_components/Mypage-Sidebar.tsx
@@ -1,23 +1,50 @@
 'use client';
 
 import Link from 'next/link';
+import MypageIcon from '@/assets/icons/mypage.svg';
+import BookmarksIcon from '@/assets/icons/like.svg';
+import ApplicationsIcon from '@/assets/icons/applications.svg';
+import SettingsIcon from '@/assets/icons/settings.svg';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 const MYPAGE_MENU_ITEMS = [
-  { label: '마이페이지', href: '/mypage' },
-  { label: '내 관심글', href: '/mypage/bookmarks' },
-  { label: '지원현황', href: '/mypage/applications' },
-  { label: '설정', href: '/mypage/settings' },
+  { label: '마이페이지', href: '/mypage', icon: <MypageIcon /> },
+  { label: '내 관심글', href: '/mypage/bookmarks', icon: <BookmarksIcon /> },
+  { label: '지원현황', href: '/mypage/applications', icon: <ApplicationsIcon /> },
+  { label: '설정', href: '/mypage/settings', icon: <SettingsIcon /> },
 ];
 
 export default function MyPageSidebar() {
+  const pathname = usePathname();
+
   return (
     <div className="bg-background-3 h-full">
-      <nav>
-        <ul>
+      <nav className="px-4 pt-[90px]">
+        <ul className="flex flex-col gap-2">
           {MYPAGE_MENU_ITEMS.map((item) => {
+            const isActive = pathname == item.href;
             return (
-              <li key={item.href}>
-                <Link href={item.href}>{item.label}</Link>
+              <li
+                key={item.href}
+                className={clsx(
+                  'text-sidebar-text hover:bg-sidebar-bg-hover flex items-center rounded-[10px] font-medium',
+                  isActive ? 'bg-sidebar-bg-active text-sidebar-text-active' : '',
+                )}
+              >
+                <Link href={item.href} className="w-full py-3">
+                  <div className={clsx('inline-flex w-full items-center gap-2 pl-2')}>
+                    <span
+                      className={clsx(
+                        'inline-flex h-4 w-4 items-center justify-center',
+                        isActive ? 'text-sidebar-icon-active' : '',
+                      )}
+                    >
+                      {item.icon}
+                    </span>
+                    {item.label}
+                  </div>
+                </Link>
               </li>
             );
           })}
--- src/app/(app)/(no-memo)/mypage/layout.tsx
+++ src/app/(app)/(no-memo)/mypage/layout.tsx
@@ -4,7 +4,7 @@ export default function MyPageLayout({ children }: { children: React.ReactNode }
   return (
     <div className="bg-background-2 flex min-h-dvh items-center justify-center px-8 py-6">
       <div className="w-full max-w-[1200px]">
-        <div className="flex h-[70vh] max-h-[780px] min-h-[560px] w-full overflow-hidden rounded-[10px] border border-gray-900 bg-white">
+        <div className="flex h-[70vh] max-h-[780px] min-h-[560px] w-full overflow-hidden rounded-[10px] border border-gray-300 bg-white">
           <aside className="w-[240px] shrink-0 border-r border-gray-300">
             <MyPageSidebar />
           </aside>
