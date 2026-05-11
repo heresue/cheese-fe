cc4578f2509a21103d9893e3ce3dad4b1c809d7a
sue <sue.synpark@gmail.com>
2026-05-11 15:19:58 +0900

    refactor: 공통 CategoryTabs 컴포넌트 아이콘 렌더링 지원 추가

--- src/components/common/CategoryTabs/type.ts
+++ src/components/common/CategoryTabs/type.ts
@@ -1,9 +1,12 @@
+import { ComponentType, SVGProps } from 'react';
+
 export type CategoryTabsSize = 'sm' | 'md';
 
 export type CategoryTabItem<TValue extends string = string> = {
   label: string;
   value: TValue;
   disabled?: boolean;
+  icon?: ComponentType<SVGProps<SVGSVGElement>>;
 };
 
 export type CategoryTabsProps<TValue extends string = string> = {
