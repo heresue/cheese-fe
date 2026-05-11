07a45e5133e8137d73471fb6d65b2d31c224542b
sue <sue.synpark@gmail.com>
2026-05-12 04:41:22 +0900

    refactor: 마이페이지 메뉴 상수를 mypage 폴더로 이동

--- src/app/(app)/(no-memo)/mypage/_components/MypageSidebar.tsx
+++ src/app/(app)/(no-memo)/mypage/_components/MypageSidebar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-import { MYPAGE_MENU_ITEMS } from '@/constants/mypageMenu';
+import { MYPAGE_MENU_ITEMS } from '../_constants/mypageMenu';
 
 import MypageIcon from '@/assets/icons/settings/user.svg';
 import BookmarksIcon from '@/assets/icons/like-outline.svg';
