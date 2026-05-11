90b279561fe53132ad61db4ff33e31570b6130e4
sue <sue.synpark@gmail.com>
2026-05-12 02:01:17 +0900

    refactor: 공통 CategoryTabs 적용 후 기존 Tabs 컴포넌트 제거

--- src/components/common/Tabs/TabList.tsx
+++
@@ -1,3 +0,0 @@
-export default function TabList({ children }: { children: React.ReactNode }) {
-  return <div className="flex gap-[7px]">{children}</div>;
-}
--- src/components/common/Tabs/type.ts
+++
@@ -1,6 +0,0 @@
-export type TabValue = string;
-
-export interface TabsContextType {
-  value: TabValue;
-  onChange: (value: TabValue) => void;
-}
