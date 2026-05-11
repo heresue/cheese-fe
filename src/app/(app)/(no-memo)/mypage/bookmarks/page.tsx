ea0ee85387aed3452fb6ddfc2deaf7269b5fc6bf
sue <sue.synpark@gmail.com>
2026-05-11 15:54:42 +0900

    refactor: 마이페이지 내 관심글 Tab을 CategoryTabs로 교체

--- src/app/(app)/(no-memo)/mypage/bookmarks/page.tsx
+++ src/app/(app)/(no-memo)/mypage/bookmarks/page.tsx
@@ -1,35 +1,56 @@
-import JobBookmarkList from '@/app/(app)/(no-memo)/mypage/bookmarks/_components/JobBookmarkList';
-import GroupBookmarkList from '@/app/(app)/(no-memo)/mypage/bookmarks/_components/GroupBookmarkList';
-import InfoBookmarkList from '@/app/(app)/(no-memo)/mypage/bookmarks/_components/InfoBookmarkList';
-import { Tab, TabList, TabPanel, Tabs } from '@/components/common/Tabs';
+'use client';
+
+import { useState } from 'react';
+
+import CategoryTabs from '@/components/common/CategoryTabs';
+import ListFilterBar from '@/components/common/ListFilterBar';
+
+import JobBookmarkList from './_components/JobBookmarkList';
+import GroupBookmarkList from './_components/GroupBookmarkList';
+import InfoBookmarkList from './_components/InfoBookmarkList';
+
+const MYPAGE_BOOKMARK_CATEGORY_TABS = [
+  { label: '채용공고', value: 'jobs' },
+  { label: '그룹모집', value: 'groups' },
+  { label: '정보/자료공유', value: 'infos' },
+];
+
+type MypageBookmarkCategoryTabValue = (typeof MYPAGE_BOOKMARK_CATEGORY_TABS)[number]['value'];
 
 export default function BookmarksPage() {
+  const [activeBookmarkTab, setActiveBookmarkTab] = useState<MypageBookmarkCategoryTabValue>(
+    MYPAGE_BOOKMARK_CATEGORY_TABS[0].value,
+  );
+
   return (
     <div>
-      <Tabs defaultValue="jobs">
-        <TabList>
-          <Tab value="jobs">채용공고</Tab>
-          <Tab value="groups">그룹모집</Tab>
-          <Tab value="infos">정보/자료공유</Tab>
-        </TabList>
+      <section>
+        <CategoryTabs
+          items={MYPAGE_BOOKMARK_CATEGORY_TABS}
+          activeValue={activeBookmarkTab}
+          onChange={setActiveBookmarkTab}
+        />
+        {/* // TODO: 커뮤니티 페이지 구현 후 ListFilterBar 컴포넌트 재사용
+        <ListFilterBar /> */}
+      </section>
 
-        <TabPanel value="jobs">
-          <div className="mt-5 flex flex-col gap-5">
-            <JobBookmarkList />
-          </div>
-        </TabPanel>
+      {activeBookmarkTab === 'jobs' && (
+        <div className="mt-5 flex flex-col gap-5">
+          <JobBookmarkList />
+        </div>
+      )}
 
-        <TabPanel value="groups">
-          <div className="mt-5">
-            <GroupBookmarkList />
-          </div>
-        </TabPanel>
-        <TabPanel value="infos">
-          <div className="mt-5">
-            <InfoBookmarkList />
-          </div>
-        </TabPanel>
-      </Tabs>
+      {activeBookmarkTab === 'groups' && (
+        <div className="mt-5">
+          <GroupBookmarkList />
+        </div>
+      )}
+
+      {activeBookmarkTab === 'infos' && (
+        <div className="mt-5">
+          <InfoBookmarkList />
+        </div>
+      )}
     </div>
   );
 }
