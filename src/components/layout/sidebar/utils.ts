648fec6b61e4d994c69eecb91be9a54db0964c15
sue <sue.synpark@gmail.com>
2026-06-13 01:05:01 +0900

    chore: 불필요한 공백 삭제

--- src/components/layout/sidebar/utils.ts
+++ src/components/layout/sidebar/utils.ts
@@ -8,7 +8,7 @@ export function getSidebarItemClassName(isActive: boolean) {
   return cn(
     'group flex w-full items-center rounded-[10px] leading-[30px] transition-colors duration-200',
     isActive
-      ? 'bg-sidebar-bg-active text-sidebar-text-active font-bold '
+      ? 'bg-sidebar-bg-active text-sidebar-text-active font-bold'
       : 'text-sidebar-text font-medium hover:bg-sidebar-bg-active hover:text-sidebar-text-active',
   );
 }
