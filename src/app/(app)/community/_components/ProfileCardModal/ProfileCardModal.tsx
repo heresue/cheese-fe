1f63d5e605d874dff1db836cf66ad5f63755f9b1
sue <sue.synpark@gmail.com>
2026-07-22 17:03:56 +0900

    chore: pr리뷰반영 - ProfileCardModal width 계산 주석 추가

--- src/app/(app)/community/_components/ProfileCardModal/ProfileCardModal.tsx
+++ src/app/(app)/community/_components/ProfileCardModal/ProfileCardModal.tsx
@@ -52,6 +52,7 @@ export default function ProfileCardModal({
 }: ProfileCardModalProps) {
   return (
     <BaseModal isOpen={isOpen} onClose={onClose} scope="content" hasOverlay>
+      {/* 100vw - sidebar (260px) - modal horizontal margins (298px) */}
       <div className="w-[calc(100vw-558px)] max-w-[1362px] min-w-[720px] overflow-hidden rounded-[10px]">
         <div className="max-h-[901px] overflow-auto">
           <div className="bg-bg-2 flex w-full flex-col gap-6 px-10 py-13">
