c3074a28734845bdcd50cd9907af7620e1707c00
sue <sue.synpark@gmail.com>
2026-07-24 01:04:04 +0900

    style: 마이페이지 수정버튼 가로사이즈 설정

--- src/app/(app)/(no-memo)/mypage/_components/Layout/SettingItem.tsx
+++ src/app/(app)/(no-memo)/mypage/_components/Layout/SettingItem.tsx
@@ -48,14 +48,13 @@ export default function SettingItem({
 
       <Button
         variant="outlineLightGray"
-        width={61}
         size={38}
         paddingX={8}
-        className={`gap-[7px] ${buttonClassName ?? ''}`}
+        className={`gap-1 tracking-[-0.02em] ${buttonClassName ?? ''}`}
         onClick={onClick}
       >
         {buttonIcon && buttonIconPosition === 'left' && (
-          <span className="inline-flex items-center justify-center">{buttonIcon}</span>
+          <span className="inline-flex h-[30px] w-5 items-center justify-center">{buttonIcon}</span>
         )}
 
         {buttonText}
