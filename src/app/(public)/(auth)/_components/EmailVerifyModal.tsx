2423af8e4ea11962996b72584c8afca6fb0008a7
sue <sue.synpark@gmail.com>
2026-08-14 04:27:59 +0900

    refactor: pr리뷰반영-이메일 인증 모달 props 범위 정리

--- src/app/(public)/(auth)/_components/EmailVerifyModal.tsx
+++ src/app/(public)/(auth)/_components/EmailVerifyModal.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import BaseModal from '@/components/common/Modal';
 import EmailVerifyForm, { EmailVerifyBaseProps } from './EmailVerifyForm';
 
-type EmailVerifyPopupProps = EmailVerifyBaseProps & {
+type EmailVerifyModalProps = Pick<EmailVerifyBaseProps, 'title' | 'description' | 'onNext'> & {
   isOpen: boolean;
   onClose: () => void;
 };
@@ -13,7 +13,7 @@ export default function EmailVerifyModal({
   title,
   description,
   onNext,
-}: EmailVerifyPopupProps) {
+}: EmailVerifyModalProps) {
   return (
     <BaseModal isOpen={isOpen} onClose={onClose} hasOverlay>
       <div className="flex w-[457px] flex-col gap-10 rounded-[25px] bg-white px-14 py-10">
